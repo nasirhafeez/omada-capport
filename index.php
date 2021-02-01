95a723c19910695d35d070b97b7db6a15de542dc
Nasir Hafeez <nasirhafeez2002@gmail.com>
2021-02-01 15:18:48 +0500

    .

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 
 session_start();
 
-$_SESSION["clientMac"] = $_GET["clientMac"];
-$_SESSION["apMac"] = $_GET["apMac"];
-$_SESSION["ssidName"] = $_GET["ssidName"];
-$_SESSION["t"] = $_GET["t"];
-$_SESSION["radioId"] = $_GET["radioId"];
-$_SESSION["site"] = $_GET["site"];
+$clientMac = $_GET["clientMac"];
+$apMac = $_GET["apMac"];
+$ssidName = $_GET["ssidName"];
+$t = $_GET["t"];
+$radioId = $_GET["radioId"];
+$site = $_GET["site"];
 
-$_SESSION["time"] = 300;
-$_SESSION["name"] = 'operator1';
-$_SESSION["password"] = 'operator1';
+$seconds = 300;
+$username = 'operator1';
+$password = 'operator1';
 
 $curl = curl_init();
 
@@ -27,8 +27,8 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS =>'{
-	"name": "operator1",
-  "password": "operator1"
+	"name": $username,
+  "password": $password
 }',
   CURLOPT_HTTPHEADER => array(
     'Content-Type: application/json'
@@ -41,11 +41,81 @@ curl_close($curl);
 
 if ($response !== false) {
   $json = json_decode($response, true);
-  echo $json[result][token];
+  $csrfToken = $json[result][token];
 }
 else {
   die("Error: check with your network administrator");
 }
+
+
+
+
+
+  // Send user to authorize and the time allowed
+$authInfo = array(
+'clientMac' => $clientMac,
+'apMac' => $apMac,
+'ssidName' => $ssidName,
+'radioId' => $radioId,
+'t' => $t,
+'time' => $seconds
+);
+
+// curl_setopt_array($curl, array(
+//   CURLOPT_URL => 'https://192.168.8.175:8043/api/v2/hotspot/login',
+//   CURLOPT_RETURNTRANSFER => true,
+//   CURLOPT_ENCODING => '',
+//   CURLOPT_MAXREDIRS => 10,
+//   CURLOPT_TIMEOUT => 0,
+//   CURLOPT_SSL_VERIFYPEER => false,
+//   CURLOPT_SSL_VERIFYHOST => false,
+//   CURLOPT_FOLLOWLOCATION => true,
+//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//   CURLOPT_CUSTOMREQUEST => 'POST',
+//   CURLOPT_POSTFIELDS =>'{
+// 	"name": "operator1",
+//   "password": "operator1"
+// }',
+//   CURLOPT_HTTPHEADER => array(
+//     'Content-Type: application/json'
+//   ),
+// ));
+
+// $ch = curl_init();
+// // post
+// curl_setopt($ch, CURLOPT_POST, TRUE);
+// // Set return to a value, not return to page
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// // Set up cookies
+// curl_setopt($ch, CURLOPT_COOKIEJAR, COOKIE_FILE_PATH);
+// curl_setopt($ch, CURLOPT_COOKIEFILE, COOKIE_FILE_PATH);
+// // Allow Self Signed Certs    
+// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+// // API Call
+// $csrfToken = self::getCSRFToken();
+// curl_setopt($ch, CURLOPT_URL, CONTROLLER_SERVER ."/extportal/". $site."/auth"."?token=".$csrfToken);
+// $data = json_encode($authInfo);
+// curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($authInfo)); 
+
+// $res = curl_exec($ch);
+// $resObj = self::resultConvert($res);
+
+// if($resObj['success'] == false){
+//  echo $res;
+//  }
+
+// curl_close($ch);
+
+
+
+
+
+
+
+
+
+
 // class TPLinkAuth
 // {
 //     function login() {       

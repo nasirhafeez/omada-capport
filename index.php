38d8b2125a63ce7e208011211460f552626ac943
Nasir Hafeez <nasirhafeez2002@gmail.com>
2021-02-01 12:29:17 +0500

    .

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 
 curl_close($curl);
-echo $resObj;
+echo $response;
 
 // $_SESSION["clientMac"] = $_GET["clientMac"];
 // $_SESSION["apMac"] = $_GET["apMac"];

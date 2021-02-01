3926f48199e5fb5024191d802fb2df9945eb0eff
Nasir Hafeez <nasirhafeez2002@gmail.com>
2021-02-01 15:39:53 +0500

    .

--- index.php
+++ index.php
@@ -53,117 +53,63 @@ else {
 
 
 
-  // Send user to authorize and the time allowed
-$authInfo = array(
-'clientMac' => $clientMac,
-'apMac' => $apMac,
-'ssidName' => $ssidName,
-'radioId' => $radioId,
-'t' => $t,
-'time' => $seconds
-);
+//   // Send user to authorize and the time allowed
+// $authInfo = array(
+// 'clientMac' => $clientMac,
+// 'apMac' => $apMac,
+// 'ssidName' => $ssidName,
+// 'radioId' => $radioId,
+// 't' => $t,
+// 'time' => $seconds
+// );
 
-$url = 'https://192.168.8.175:8043/api/v2/hotspot/extportal'.$site."/auth"."?token=".$csrfToken;
+// $url = 'https://192.168.8.175:8043/api/v2/hotspot/extportal'.$site."/auth"."?token=".$csrfToken;
 
-$curl = curl_init();
+// $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_SSL_VERIFYPEER => false,
-  CURLOPT_SSL_VERIFYHOST => false,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => json_encode($authInfo),
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/json'
-  ),
-));
+// curl_setopt_array($curl, array(
+//   CURLOPT_URL => $url,
+//   CURLOPT_RETURNTRANSFER => true,
+//   CURLOPT_ENCODING => '',
+//   CURLOPT_MAXREDIRS => 10,
+//   CURLOPT_TIMEOUT => 0,
+//   CURLOPT_SSL_VERIFYPEER => false,
+//   CURLOPT_SSL_VERIFYHOST => false,
+//   CURLOPT_FOLLOWLOCATION => true,
+//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//   CURLOPT_CUSTOMREQUEST => 'POST',
+//   CURLOPT_POSTFIELDS => json_encode($authInfo),
+//   CURLOPT_HTTPHEADER => array(
+//     'Content-Type: application/json'
+//   ),
+// ));
 
-// $ch = curl_init();
-// // post
-// curl_setopt($ch, CURLOPT_POST, TRUE);
-// // Set return to a value, not return to page
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-// // Set up cookies
-// curl_setopt($ch, CURLOPT_COOKIEJAR, COOKIE_FILE_PATH);
-// curl_setopt($ch, CURLOPT_COOKIEFILE, COOKIE_FILE_PATH);
-// // Allow Self Signed Certs    
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-// // API Call
-// $csrfToken = self::getCSRFToken();
-// curl_setopt($ch, CURLOPT_URL, CONTROLLER_SERVER ."/extportal/". $site."/auth"."?token=".$csrfToken);
-// $data = json_encode($authInfo);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($authInfo)); 
+// // $ch = curl_init();
+// // // post
+// // curl_setopt($ch, CURLOPT_POST, TRUE);
+// // // Set return to a value, not return to page
+// // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+// // // Set up cookies
+// // curl_setopt($ch, CURLOPT_COOKIEJAR, COOKIE_FILE_PATH);
+// // curl_setopt($ch, CURLOPT_COOKIEFILE, COOKIE_FILE_PATH);
+// // // Allow Self Signed Certs    
+// // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+// // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+// // // API Call
+// // $csrfToken = self::getCSRFToken();
+// // curl_setopt($ch, CURLOPT_URL, CONTROLLER_SERVER ."/extportal/". $site."/auth"."?token=".$csrfToken);
+// // $data = json_encode($authInfo);
+// // curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($authInfo)); 
 
-$res = curl_exec($curl);
-// $resObj = self::resultConvert($res);
+// $res = curl_exec($curl);
+// // $resObj = self::resultConvert($res);
 
-// if($resObj['success'] == false){
-//  echo $res;
-//  }
+// // if($resObj['success'] == false){
+// //  echo $res;
+// //  }
 
-curl_close($curl);
+// curl_close($curl);
 
-echo $res;
-
-
-
-
-
-
-
-
-// class TPLinkAuth
-// {
-//     function login() {       
-//     $ch = curl_init();
-//     // post
-//     curl_setopt($ch, CURLOPT_POST, TRUE);
-//     // Set return to a value, not return to page
-//     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//     // Set up cookies
-//     curl_setopt($ch, CURLOPT_COOKIEJAR, "cookies.txt");
-//     curl_setopt($ch, CURLOPT_COOKIEFILE, "cookies.txt");
-//     // Allow Self Signed Certs
-//     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-//     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-//     // API Call
-//     curl_setopt($ch, CURLOPT_URL, "https://192.168.8.175:8043/api/v2/hotspot/login");
-//     curl_setopt($ch, CURLOPT_POSTFIELDS, "name=" . "operator1" ."&password=" . "operator1");
-//         $res = curl_exec($ch);
-//         $resObj = json_decode($res);
-//         echo $res;
-//     //Prevent CSRF
-//                 // if($resObj->success == true){
-//                 //     echo "setting csrf token";
-//                 //                 self::setCSRFToken($resObj->value);
-//                 // }
-//     curl_close($ch);
-//     }
-
-//     private static function setCSRFToken($token) {
-//                     $myfile = fopen("../token.txt", "w") or die("Unable to open file!");
-//                     fwrite($myfile, $token);
-//                     fclose($myfile);
-//                     return $token;
-//     }
-// }
-
-// try { 
-//     $tplink_C = new TPLinkAuth();
-// echo "try 2";
-//     $user_info = $tplink_C->login();
-//     echo json_encode($user_info);
-
-// } catch (Exception $e) {
-//     echo $e->getMessage();
-//     exit;
-// }
+// echo $res;
 
 ?>
